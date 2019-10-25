1fa894e2e89fca380103f86b2b0ac3180ef30c08
Carlos Pineda <carlos.r.pineda@gmail.com>
2019-10-25 04:33:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,10 @@
 # My Code here....
-def like_map_to_negative(array)
+def map_to_negativize(array)
 
-new_array = map_to_negativize(array)
+new_array = []
+
+array.each { |element|
+              new_array.push(element *= -1) 
 
 return new_array
 
