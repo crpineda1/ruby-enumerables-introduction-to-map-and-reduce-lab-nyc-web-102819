089ba106bfd8f68bf413b1b3f98a3a38a8a0e532
Carlos Pineda <carlos.r.pineda@gmail.com>
2019-10-25 04:34:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,7 +4,7 @@ def map_to_negativize(array)
 new_array = []
 
 array.each { |element|
-              new_array.push(element *= -1) 
+              new_array.push(element *= -1) }
 
 return new_array
 
