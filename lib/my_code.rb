5769906fb171c16aeb261b46682780db103d4c45
Carlos Pineda <carlos.r.pineda@gmail.com>
2019-10-25 04:38:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,13 +1,20 @@
 # My Code here....
 def map_to_negativize(array)
 
-new_array = []
+  new_array = []
+  array.each { |element|
+                new_array.push(element *= -1) }
 
+  return new_array
+end
+
+def map_to_no_change(array)
+
+new_array = []
 array.each { |element|
-              new_array.push(element *= -1) }
+              new_array.push(element) }
 
 return new_array
-
 end
 
 def map_to_double(array)
